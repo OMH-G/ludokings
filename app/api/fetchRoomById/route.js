d86cd8b56fdf1660af9e5acaea55d9110bd36f4b
akash <trimbakeakash5@gmail.com>
2023-09-22 11:51:22 +0530

    fetch room by id

--- app/api/fetchRoomById/route.js
+++ app/api/fetchRoomById/route.js
@@ -1,38 +1,3 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import axios from "axios";
-
-// export async function POST(NextRequest) {
-//   try {
-//     const reqBody = await NextRequest.json();
-//     const { roomID } = reqBody;
-//     const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-//     const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-//     const tableName2 = "User";
-//     const apiUrl = `${supabaseUrl}/rest/v1/${tableName2}?select=user_id&roomid=eq.${roomID}`;
-
-//     const response = await axios.get(apiUrl, {
-//       headers: {
-//         apikey: supabaseKey,
-//       },
-//     });
-
-//     if (response.status === 200) {
-//       //   return response.data;
-
-//       return NextResponse.json({
-//         data: response,
-//       });
-//     }
-
-//     // return NextResponse.json({
-//     //   message: roomID,
-//     // });
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     return null;
-//   }
-// }
-
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 
@@ -43,6 +8,7 @@ export async function POST(NextRequest) {
     const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
     const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
     const tableName2 = "User";
+    // have to change the room id
     const apiUrl = `${supabaseUrl}/rest/v1/${tableName2}?select=user_id&roomid=eq.${10}`;
 
     const response = await axios.get(apiUrl, {
