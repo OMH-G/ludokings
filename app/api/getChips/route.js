d83bce0a9d1a143bb2f357977a92e9f9c2b6ac8b
akash <trimbakeakash5@gmail.com>
2023-09-25 19:50:02 +0530

    added addmoney1 status to backend10

--- app/api/getChips/route.js
+++ app/api/getChips/route.js
@@ -1,17 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import axios from "axios";
 import { getChips } from "@/supabaseClient";
 
 export async function POST(NextRequest) {
   try {
     const reqBody = await NextRequest.json();
     const { id } = reqBody;
-    if (id) {
-      const userChips = await getChips(id);
 
-      return NextResponse.json(userChips);
+    if (!id) {
+      return NextResponse.json({ error: "Missing userId" }, { status: 400 });
     }
+
+    const userChips = await getChips(id);
+
+    return NextResponse.json(userChips);
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json(
+      { error: "An error occurred at getChips server: " + error.message },
+      { status: 500 }
+    );
   }
 }
