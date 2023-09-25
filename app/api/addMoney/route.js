23d60faf032f8c5d20470b31cb5b410f7628917a
akash <trimbakeakash5@gmail.com>
2023-09-25 16:23:45 +0530

    added addroute to backend

--- app/api/addMoney/route.js
+++ app/api/addMoney/route.js
@@ -6,12 +6,12 @@ export async function POST(NextRequest) {
   try {
     const reqBody = await NextRequest.json();
     const { userId, amount } = reqBody;
-    if (userId) {
+    if (userId && amount) {
       const userChips = await getChips(userId);
       let final = userChips + amount;
       await updateChips(userId, final);
 
-      return NextResponse.json("Chips added to the wallet successfully.");
+      return NextResponse.json({ message: "Chips added successfully" });
     }
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
