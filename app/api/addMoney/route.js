3f5734d9f8b9cf14cc876b9a1c5427ef2ab38feb
akash <trimbakeakash5@gmail.com>
2023-09-25 16:48:40 +0530

    added addmoney1 status to backend2

--- app/api/addMoney/route.js
+++ app/api/addMoney/route.js
@@ -5,13 +5,13 @@ export async function POST(NextRequest) {
   try {
     const reqBody = await NextRequest.json();
     const { userId, amount } = reqBody;
-    if (userId && amount) {
-      const userChips = await getChips(userId);
-      let final = userChips + amount;
-      await updateChips(userId, final);
+    // if (userId && amount) {
+    const userChips = await getChips(userId);
+    let final = userChips + amount;
+    await updateChips(userId, final);
 
-      return NextResponse.json({ chips: final }, { status: 200 });
-    }
+    return NextResponse.json({ chips: final }, { status: 200 });
+    // }
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
