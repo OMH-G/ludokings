f15cda5c86f813fe811a1ea69be6c9849f97c99d
akash <trimbakeakash5@gmail.com>
2023-09-28 23:47:59 +0530

    chips calculation

---
+++ app/api/gameResult/route.js
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { updateChips, getChips, fetchRoomsById } from "@/supabaseClient";
+
+export async function POST(NextRequest) {
+  try {
+    const reqBody = await NextRequest.json();
+    const { hasCongratulations, userId, roomId, roomValue } = reqBody;
+
+    let response;
+    if (hasCongratulations == true) {
+      const userChips = await getChips(userId);
+      let final = userChips + roomValue * 0.95;
+      const res = await updateChips(userId, final);
+      response = res;
+    } else {
+      const userChips = await getChips(userId);
+      let final = userChips - roomValue;
+      const res = await updateChips(userId, final);
+      response = res;
+    }
+
+    return NextResponse.json(response);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error:
+          "An error occurred at fetchRoomOwnerById server: " + error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
